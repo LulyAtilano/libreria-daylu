8b75ae8d2e130d7b1cb3320e9e88f05c29da0edc
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-07-20 09:48:33 -0500

    Funcion corregida para obtener un array de objetos con los url del MD

--- js/app.js
+++ js/app.js
@@ -1,4 +1,133 @@
-const fs = require('fs');
+function getUrlsMd(markdown){
+    let regex = /!?\[([^\[.]+?)\]\((.+?)\)/g
+
+    let foundMatches = [];
+    let match=[];
+
+    while( ( match = regex.exec(markdown) ) !== null) {
+      if (match[0].startsWith("!")){ continue;}
+
+      var match1 = {
+        'href' : match[2],
+        'text': match[1]
+      }
+
+      foundMatches.push(match1);
+
+    }
+    return foundMatches;
+  }
+
+  var links = getUrlsMd(`# Lorem ipsum
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+  incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et
+  [dolore](https://en.wiktionary.org/wiki/dolore) magna aliqua. Ut enim ad minim
+  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat.
+
+  [foo](http://foo.com)
+
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+  culpa qui officia deserunt mollit anim id est laborum.`
+
+  );
+  console.log(links);
+
+
+
+
+
+
+
+
+
+
+/*//Buscar http en string
+var content;
+
+
+const textGiven =`# Lorem ipsum
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et
+[dolore](https://en.wiktionary.org/wiki/dolore) magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat.
+
+[foo](http://foo.com)
+
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.`
+
+//Señalar elementos que identifican un url
+var searchUrl = /\[.+\]\(.+\)/g;
+
+var urlFound = textGiven.match(searchUrl);
+console.log(urlFound)
+
+
+
+console.log(urlFound[0]); //Es un array de un solo index
+//Separar array url found en name y link
+
+//Dividir nombre de url
+/*function divideStringTittle (){
+
+    var getTittle = /\[\]/g;
+    var urlTittle = urlFound.match(getTittle);
+    console.log(getTittle)
+}
+
+divideStringTittle ();
+
+//Dividir link del url
+function divideStringLink (){
+
+    var getLink = ^(http|https):\/\ $/g;  //No sirve linea rejex , opcion 2: /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/ opcion3: https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
+    var urlLink = urlFound.match(getLink);
+    console.log(getLink)
+}
+
+divideStringLink ();
+
+//Asignar getTittle como key, getLink como value y convertir a objeto textGivenUrl
+
+/*var textGivenUrl = {
+   'name': 'urlTittle',
+   'link': 'urlLink'
+}*/
+
+/*var textGivenUrl1 = {
+    textGiven['name'] = '',
+    textGiven['link'] = ''
+}
+
+console.log(textGivenUrl1)
+
+var textGivenUrl2 = {
+    textGiven['name'] = '',
+    textGiven['link'] = ''
+}
+
+console.log(textGivenUrl2)
+
+//Hacer array con los dos objetos
+
+
+
+
+
+
+
+
+
+
+
+
+/*const fs = require('fs');
 
 fs.readFile("../md/README.md", "utf-8", (err, data) => {
    if(err) {
@@ -11,18 +140,9 @@ fs.readFile("../md/README.md", "utf-8", (err, data) => {
 
   function getUrl (){
       var searchUrl = /\[.+\]\(.+\)/g;
-      var getUrl = textGiven.match(searchUrl);
-      console.log(getUrl);
   }
+});*/
 
-
-});
-
-
-
-//const fs = require('fs');  //  *./.md require import
-
-//Obtener texto de markdown e imprimilo en consola gitbash
 
 /*var textSample = "textSample.md";
 
@@ -57,32 +177,6 @@ var searchUrl = /\[.+\]\(.+\)/g;
 
 var getUrl = textGiven.match(searchUrl);
 console.log(getUrl);
-
-
-$(document).ready(function(){
-    console.log(data);
-
-    function initialize() {
-       var  locations=Object.keys(data);
-        console.log(locations);
-    }
-
-   function locationDetail(locationName){
-       var locationObj = data[name];
-       var generations = Object.keys(locationObj);
-       generations.forEach(function (item, index){
-           var generation =locationObj[item];
-           var students= generation.students;
-
-       });
-
-   };
-   initialize ();
-
-
-
-
-
 
 
 /*const fs = require('fs');

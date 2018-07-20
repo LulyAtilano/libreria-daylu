1a242170d81423af60d9b691bccd2dd1072c71f4
latilanoh@gmail.com <latilanoh@gmail.com>
2018-07-20 12:22:53 -0500

    se eliminan archivos que no se necesitan, se crea subcarpeta modules en lib para crear los archivos con funciones, se actualiza el archivo gitignore

--- src/modules/getUrlsMd.js
+++ src/modules/getUrlsMd.js
@@ -7,7 +7,7 @@ function getUrlsMd(markdown){
     while( ( match = regex.exec(markdown) ) !== null) {
       if (match[0].startsWith("!")){ continue;}
 
-      var match1 = {
+      let match1 = {
         'href' : match[2],
         'text': match[1]
       }
@@ -16,9 +16,50 @@ function getUrlsMd(markdown){
 
     }
     return foundMatches;
+}
+
+
+var links = getUrlsMd(`# Lorem ipsum
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et
+[dolore](https://en.wiktionary.org/wiki/dolore) magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat.
+
+[foo](http://foo.com)
+
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.`
+
+);
+console.log(links);*/
+
+module.exports = getUrlsMd;
+
+/*( function(){ // función anónima autoinvocada
+
+  const allanPoe = (number) => {
+      let crow = "";
+
+      for (let i = 0; i < number ; i++ ){
+          crow += "nunca más ";
+      }
+      return crow;
+  };
+
+  if ( typeof window == "undefined" ) {
+      console.log('consola');
+      module.exports = allanPoe;
+  } else {
+      console.log('navegador');
+      window.allanPoe = allanPoe;
   }
 
-  var links = getUrlsMd(`# Lorem ipsum
+})();*/
+
+const links = getUrlsMd(`# Lorem ipsum
 
   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
   incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et
@@ -32,19 +73,11 @@ function getUrlsMd(markdown){
   fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
   culpa qui officia deserunt mollit anim id est laborum.`
 
-  );
-  console.log(links);
+);
+console.log(links);
 
-
-
-
-
-
-
-
-
-
-/*//Buscar http en string
+/*
+//Buscar http en string
 var content;
 
 
